da97381fac73050011e120894fbc9000f14157ee
Mait Märdin <mait.mardin@guardtime.com>
2024-04-22 16:04:18 +0300

    Add orchestration txsystem.

---
+++ txsystem/orchestration/attributes.go
@@ -0,0 +1,35 @@
+package orchestration
+
+import "github.com/alphabill-org/alphabill-go-sdk/types"
+
+const (
+	PayloadTypeAddVAR = "addVar" // add validator assignment record
+)
+
+const DefaultSystemID types.SystemID = 0x00000004
+
+type (
+	AddVarAttributes struct {
+		_   struct{} `cbor:",toarray"`
+		Var ValidatorAssignmentRecord
+	}
+
+	ValidatorAssignmentRecord struct {
+		_                      struct{} `cbor:",toarray"`
+		EpochNumber            uint64
+		EpochSwitchRoundNumber uint64 // root chain round number
+		ValidatorAssignment    ValidatorAssignment
+	}
+
+	ValidatorAssignment struct {
+		_          struct{} `cbor:",toarray"`
+		Validators []ValidatorInfo
+		QuorumSize uint64 // total amount of staked Alpha required to reach consensus
+	}
+
+	ValidatorInfo struct {
+		_           struct{} `cbor:",toarray"`
+		ValidatorID []byte   // validator public key used to sign validation messages
+		Stake       uint64   // total amount of staked Alpha by the validator
+	}
+)
