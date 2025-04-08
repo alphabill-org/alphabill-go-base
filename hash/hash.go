3274c16bc53882c7ca03b9cb7362d41821a45c00
Ain <ain.valtin@guardtime.com>
2025-04-08 12:20:10 +0300

    use sha256 from stdlib

--- hash/hash.go
+++ hash/hash.go
@@ -2,21 +2,8 @@ package hash
 
 import (
 	"crypto"
-	"crypto/sha256"
 	"fmt"
 )
-
-var Zero256 = make([]byte, sha256.Size)
-
-// Sum256 returns the SHA256 checksum of the data using the MessageHash augmented hashing.
-func Sum256(data []byte) []byte {
-	// return zero hash in case data is either empty or missing
-	if len(data) == 0 {
-		return Zero256
-	}
-	hsh := sha256.Sum256(data)
-	return hsh[:]
-}
 
 func NewSha256() *Hash {
 	return New(crypto.SHA256.New())
