312df25b62bcb6618a3dc241cb1b9c1653497698
Ain <ain.valtin@guardtime.com>
2025-04-14 15:13:21 +0300

    refactor uint utils

--- util/math_util_test.go
+++ util/math_util_test.go
@@ -7,42 +7,148 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSafeAddOverflow(t *testing.T) {
+func TestAddUint64(t *testing.T) {
 	t.Parallel()
-	cases := []struct {
-		a      uint64
-		b      uint64
-		result uint64
-		ok     bool
-	}{
-		{1, 1, 2, true},
-		{math.MaxUint64, 1, 0, false},
-		{math.MaxUint64, 0, math.MaxUint64, true},
-		{math.MaxUint64 - 1, 1, math.MaxUint64, true},
-	}
-	for _, tt := range cases {
-		result, ok := SafeAdd(tt.a, tt.b)
-		require.Equal(t, tt.ok, ok)
-		require.Equal(t, tt.result, result)
-	}
+
+	t.Run("OK", func(t *testing.T) {
+		cases := []struct {
+			input  []uint64
+			result uint64
+		}{
+			{nil, 0},
+			{[]uint64{}, 0},
+			{[]uint64{1}, 1},
+			{[]uint64{1, 2}, 3},
+			{[]uint64{0, 0, 0}, 0},
+			{[]uint64{0, 1, 0}, 1},
+			{[]uint64{2, 1, 3}, 6},
+			{[]uint64{math.MaxUint64, 0}, math.MaxUint64},
+			{[]uint64{math.MaxUint64 - 2, 1, 1}, math.MaxUint64},
+		}
+
+		for x, tt := range cases {
+			sum, ok := AddUint64(tt.input...)
+			if !ok {
+				t.Errorf("unexpected overflow for test case %d", x)
+				continue
+			}
+			if sum != tt.result {
+				t.Errorf("[%d] expected sum %d, got %d", x, tt.result, sum)
+			}
+		}
+	})
+
+	t.Run("overflow", func(t *testing.T) {
+		cases := [][]uint64{
+			{math.MaxUint64, 1},
+			{math.MaxUint64, 1, 1},
+			{1, math.MaxUint64},
+			{1, 1, math.MaxUint64 - 1},
+			{math.MaxUint64, math.MaxUint64, math.MaxUint64},
+		}
+
+		for x, tt := range cases {
+			if sum, ok := AddUint64(tt...); ok {
+				t.Errorf("expected overflow for test case %d, got %d", x, sum)
+			}
+		}
+	})
 }
 
-func TestSafeSubUnderflow(t *testing.T) {
+func TestSafeAdd(t *testing.T) {
 	t.Parallel()
-	cases := []struct {
-		a      uint64
-		b      uint64
-		result uint64
-		ok     bool
-	}{
-		{2, 1, 1, true},
-		{math.MaxUint64, math.MaxUint64, 0, true},
-		{math.MaxUint64 - 1, math.MaxUint64, 0, false},
-		{1, 2, 0, false},
-	}
-	for _, tt := range cases {
-		result, ok := SafeSub(tt.a, tt.b)
-		require.Equal(t, tt.ok, ok)
-		require.Equal(t, tt.result, result)
-	}
+
+	t.Run("OK", func(t *testing.T) {
+		cases := []struct {
+			a      uint64
+			b      uint64
+			result uint64
+		}{
+			{0, 0, 0},
+			{0, 1, 1},
+			{1, 0, 1},
+			{1, 1, 2},
+			{math.MaxUint32, math.MaxUint32, 0x01_fffffffe},
+			{math.MaxUint64, 0, math.MaxUint64},
+			{0, math.MaxUint64, math.MaxUint64},
+			{math.MaxUint64 - 1, 1, math.MaxUint64},
+			{1, math.MaxUint64 - 1, math.MaxUint64},
+		}
+
+		for _, tt := range cases {
+			result, ok := SafeAdd(tt.a, tt.b)
+			if !ok {
+				t.Errorf("unexpected overflow for %x + %x = %x", tt.a, tt.b, tt.result)
+				continue
+			}
+			if result != tt.result {
+				t.Errorf("%x + %x = %x (expected %x)", tt.a, tt.b, result, tt.result)
+			}
+		}
+	})
+
+	t.Run("overflow", func(t *testing.T) {
+		cases := []struct {
+			a uint64
+			b uint64
+		}{
+			{math.MaxUint64, 1},
+			{1, math.MaxUint64},
+			{math.MaxUint64 - 1, 2},
+			{math.MaxUint64, math.MaxUint64},
+		}
+
+		for _, tt := range cases {
+			if result, ok := SafeAdd(tt.a, tt.b); ok {
+				t.Errorf("expected overflow for %x + %x got %x", tt.a, tt.b, result)
+			}
+		}
+	})
+}
+
+func TestSafeSub(t *testing.T) {
+	t.Parallel()
+
+	t.Run("OK", func(t *testing.T) {
+		cases := []struct {
+			a      uint64
+			b      uint64
+			result uint64
+		}{
+			{0, 0, 0},
+			{1, 1, 0},
+			{2, 1, 1},
+			{math.MaxUint64, 1, math.MaxUint64 - 1},
+			{math.MaxUint64, math.MaxUint64 - 1, 1},
+			{math.MaxUint64, math.MaxUint64, 0},
+		}
+
+		for _, tt := range cases {
+			result, ok := SafeSub(tt.a, tt.b)
+			if !ok {
+				t.Errorf("unexpected underflow for %x - %x", tt.a, tt.b)
+				continue
+			}
+			require.Equal(t, tt.result, result, "%x - %x = %x", tt.a, tt.b, result)
+		}
+	})
+
+	t.Run("underflow", func(t *testing.T) {
+		cases := []struct {
+			a uint64
+			b uint64
+		}{
+			{0, 1},
+			{1, 2},
+			{0, math.MaxUint64},
+			{1, math.MaxUint64},
+			{math.MaxUint64 - 1, math.MaxUint64},
+		}
+
+		for _, tt := range cases {
+			if result, ok := SafeSub(tt.a, tt.b); ok {
+				t.Errorf("expected underflow for %x - %x got %x", tt.a, tt.b, result)
+			}
+		}
+	})
 }
